4c612f7b9f8ae24f2311e4932ec5daddfe7b55ec
Hampus Persson <hampus@hampuspersson.se>
2013-05-10 17:53:08 +0200

    Removed log message

--- javascripts/script.js
+++ javascripts/script.js
@@ -5,7 +5,6 @@
 (function($) {
 	$('#login-button').on('click', function() {
 		var position = $(this).offset();
-		console.log(position);
 		var props = {
 			left: position.left,
 			top: position.top + $(this).height
