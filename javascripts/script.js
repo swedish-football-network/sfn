46efbc262e3012aa341df2671fcb144680f6e814
Hampus Persson <hampus@hampuspersson.se>
2013-05-10 20:12:53 +0200

    Hide modal on click outside

--- javascripts/script.js
+++ javascripts/script.js
@@ -3,12 +3,21 @@
 |------------------------------------------------------------------------*/
 
 (function($) {
-	$('#login-button').on('click', function() {
+	$('#login-button').on('click', function(e) {
+		e.stopPropagation();
 		var position = $(this).offset();
 		var props = {
 			left: position.left,
 			top: position.top + $(this).height() + 25 //Buttons position + height + height of css arrow and its padding
 		};
 		$('#login-modal').css(props).toggle();
+	});
+
+	$('#login-modal').on('click', function(e) {
+		e.stopPropagation(); // Prevent click on the modal to cause the modal to hide
+	});
+
+	$('body').click(function(e){
+    $('#login-modal').hide();
 	});
 }) (jQuery);
